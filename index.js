8f0b5495d3902cc778847923f5236e3a251da8e1
Lahari <lahu.manchik@ufl.edu>
2016-07-19 19:51:33 -0400

    added status codes to the return of the hourly forecast

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var server = express();
+var $http = require('axios');
 
 var port = process.env.PORT || 8080;
+var apiKey = require('./config').apiKey; //allows to not expose key
+var baseUrl = 'https://api.forecast.io/forecast/';
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 
-server.get('/', function(req, res){
-  res.send('hello');
+server.get('/forecast/hourly/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/' + req.params.lat+ ',' + req.params.lon)
+    .then(function(response){
+      var resObj = {
+        latitude: response.data.latitude,
+        longitude: response.data.longitude,
+        hourly: response.data.hourly,
+
+      };
+      res.status(200).json(resObj);
+    })
+    .catch(function(error){
+    res.status(500).json({
+        msg: error
+    });
+  });
 });
 
 server.listen(port, function(){

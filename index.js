f496d127e4affe8cb63b44ca829112515bbdaa93
Lahari <lahu.manchik@ufl.edu>
2016-07-19 16:11:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var server = express();
+
+var port = process.env.PORT || 8080;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(cors());
+
+server.get('/', function(req, res){
+  res.send('hello');
+});
+
+server.listen(port, function(){
+  console.log('Now running on port...' , port);
+});
